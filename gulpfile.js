d5b6e50c8cbf1d24d166428dd9dfe1afdb10a0e7
sato <sato@tsumikiinc.com>
2015-04-01 18:06:48 +0900

    gulpfile.jsコメント追加、tasks/sass.js追加

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ requireDir('./tasks');
 
 reload = browserSync.reload;
 
+// ローカルサーバ
 gulp.task('serve', function() {
   return browserSync({
     notify: false,
@@ -24,12 +25,37 @@ gulp.task('serve', function() {
   });
 });
 
-gulp.task('start', sequence(['jade', 'stylus'], ['browserify'], 'serve'));
+// 'default' タスク実行前に処理しておきたいタスク
+gulp.task('start', sequence([
+  'jade',
+  'stylus'/* or 'sass' */,
+  'browserify'
+], 'serve'));
 
+// 作業開始
+// ファイルの変更監視で対象タスク実行とブラウザのオートリロード
 gulp.task('default', ['start'], function() {
   gulp.watch(['./' + CONFIG.SRC + '/**/*.coffee'], ['browserify', reload]);
   gulp.watch(['./' + CONFIG.SRC + '/**/*.jade'], ['jade', reload]);
   gulp.watch(['./' + CONFIG.SRC + '/**/*.styl'], ['stylus', reload]);
+  gulp.watch(['./' + CONFIG.SRC + '/**/*.{scss,sass}'], ['sass', reload]);
 });
 
-gulp.task('build', sequence('clean', ['jade', 'stylus'], 'copy', ['replace-min', 'minify-css-jp', 'minify-css', 'browserify', 'imagemin'], 'uglify'));
+// ビルド
+// 適宜変更
+// sequence　で並列処理と直列処理を区別
+gulp.task('build', sequence(
+  'clean',
+  [
+    'jade',
+    'stylus'
+  ],
+  'copy',
+  [
+    'replace-min',
+    'minify-css',
+    'browserify',
+    'imagemin'
+  ],
+  'uglify'
+));
