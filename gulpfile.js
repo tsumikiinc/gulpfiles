cb558384586fa79604d7445c325cc6eeef9dc6e0
sato <sato@tsumikiinc.com>
2015-04-13 20:01:52 +0900

    js監視をwatchifyに変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var sequence = require('gulp-sequence');
+var runSequence = require('run-sequence');
 var requireDir = require('require-dir');
 var CONFIG = require('./package.json').projectConfig;
 
@@ -26,36 +26,48 @@ gulp.task('serve', function() {
 });
 
 // 'default' タスク実行前に処理しておきたいタスク
-gulp.task('start', sequence([
-  'jade',
-  'stylus'/* or 'sass' */,
-  'browserify'
-], 'serve'));
+gulp.task('start', function(cb) {
+  runSequence(
+    [
+      'jade',
+      'stylus'/* or 'sass' */,
+      'browserify'
+    ],
+    'watchify',
+    'serve',
+    cb
+  );
+});
 
 // 作業開始
 // ファイルの変更監視で対象タスク実行とブラウザのオートリロード
 gulp.task('default', ['start'], function() {
-  gulp.watch(['./' + CONFIG.SRC + '/**/*.coffee'], ['browserify', reload]);
   gulp.watch(['./' + CONFIG.SRC + '/**/*.jade'], ['jade', reload]);
   gulp.watch(['./' + CONFIG.SRC + '/**/*.styl'], ['stylus', reload]);
-  gulp.watch(['./' + CONFIG.SRC + '/**/*.{scss,sass}'], ['sass', reload]);
+  // or ↓
+  // gulp.watch(['./' + CONFIG.SRC + '/**/*.{scss,sass}'], ['sass', reload]);
+
+  gulp.watch(['./' + CONFIG.DST + '/**/*.js'], reload);
 });
 
 // ビルド
 // 適宜変更
 // sequence　で並列処理と直列処理を区別
-gulp.task('build', sequence(
-  'clean',
-  [
-    'jade',
-    'stylus'
-  ],
-  'copy',
-  [
-    'replace-min',
-    'minify-css',
-    'browserify',
-    'imagemin'
-  ],
-  'uglify'
-));
+gulp.task('build', function(cb) {
+  runSequence(
+    'clean',
+    [
+      'jade',
+      'stylus'/* or 'sass' */,
+    ],
+    'copy',
+    [
+      'replace-min',
+      'minify-css',
+      'browserify',
+      'imagemin'
+    ],
+    'uglify',
+    cb
+  );
+});
